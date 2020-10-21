95ca40bd6c574fa31196d989ba28ff049414c160
JRodDvlpr <Mialienproductions@gmail.com>
2020-10-20 21:31:35 -0400

    working on grid

---
+++ game-of-life/src/components/Main.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+// Package that uses a function produce to act as a double buffer by allowing you  to return unmutable state
+
+import produce from 'immer';
+
+const Main = () => {
+    return(
+        <></>
+    )
+}
+
+export default Main;
