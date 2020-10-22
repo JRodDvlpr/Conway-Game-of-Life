8ca99f40b2342a023961e4b92e36bba8fcbb5162
JRodDvlpr <Mialienproductions@gmail.com>
2020-10-21 23:26:20 -0400

    grid added

--- src/components/Main.js
+++ src/components/Main.js
@@ -99,31 +99,32 @@ const Main = () => {
 			return
 		}
 		
-		setGrid((k) => {
-			return produce(k,(gridDup) => {
-				for (let n = 0; n < nRows; n++) {
-					for (let i = 0; i < nCols; i++) {
-						let neighbors = 0
-                        // Checks neighbors and switches between life and death
-						// eslint-disable-next-line
-						operations.forEach(([x, y]) => {
-							const newN = n + x
-							const newI = i + y
-							//#Makes Grid Stop at Edges
-							if (newN >= 0 && newN < nRows && newI >= 0 && newI < nCols) {
-								neighbors += k[newN][newI]
-							}
-						})
-						if (neighbors < 2 || neighbors > 3) {
-							gridDup[n][i] = 0
-						} else if (k[n][i] === 0 && neighbors === 3) {
-							gridDup[n][i] = 1
-						}
-					}
+		setGrid(g => {
+            return produce(g, gridDup => {
+              for (let i = 0; i < nRows; i++) {
+                for (let k = 0; k < nCols; k++) {
+                  let neighbors = 0;
 
-					}
-		})
-    })
+                    // Checks neighbors and switches between life and death
+					// eslint-disable-next-line
+                  operations.forEach(([x, y]) => {
+                    const newI = i + x;
+                    const newK = k + y;
+                    if (newI >= 0 && newI < nRows && newK >= 0 && newK < nCols) {
+                      neighbors += g[newI][newK];
+                    }
+                  });
+      
+                  if (neighbors < 2 || neighbors > 3) {
+                    gridDup[i][k] = 0;
+                  } else if (g[i][k] === 0 && neighbors === 3) {
+                    gridDup[i][k] = 1;
+                  }
+                }
+            }
+        });
+    });
+
     setTimeout(runSimulation, nSecs, setGen((prevCount) => prevCount + 1))
   }, [])
     
@@ -133,6 +134,33 @@ const Main = () => {
         <>
         <div>{gen}</div>
         <div><h1>Section</h1></div>
+
+        <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: `repeat(${nCols}, 20px)`
+        }}
+      >
+        {grid.map((gridRows, i) =>
+          gridRows.map((col, k) => (
+            <div
+              key={`${i}-${k}`}
+              onClick={() => {
+                const newGrid = produce(grid, gridDup => {
+                  gridDup[i][k] = grid[i][k] ? 0 : 1;
+                });
+                setGrid(newGrid);
+              }}
+              style={{
+                width: 20,
+                height: 20,
+                backgroundColor: grid[i][k] ? "pink" : undefined,
+                border: "solid 1px black"
+              }}
+            />
+          ))
+        )}
+      </div>
         </>
     )
 }
